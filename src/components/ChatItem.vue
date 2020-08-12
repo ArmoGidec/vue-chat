57676397063ec86d643bf1c2b627d3acb2c1377a
diatomiccoder <chudak_93@live.com>
2020-08-12 22:08:53 +0500

    make ChatItem component functional

--- src/components/ChatItem.vue
+++ src/components/ChatItem.vue
@@ -1,10 +1,13 @@
-<template>
+<template functional>
     <div class="chat-item">
         <div class="chat-item__title">
-            {{ item.subject }} <span class="chat-item__created-mark">{{ item.created | toDateString }}</span>
+            {{ props.item.subject }}
+            <span class="chat-item__created-mark">{{
+                props.item.created | toDateString
+            }}</span>
         </div>
         <p class="chat-item__message">
-            {{ item.part | truncate(100) }}
+            {{ props.item.part | truncate(100) }}
         </p>
     </div>
 </template>
@@ -26,12 +29,12 @@ export default {
     &__title {
         display: flex;
         justify-content: space-between;
-        color: #35383D;
+        color: #35383d;
         font-weight: 500;
     }
 
     &__created-mark {
-        color: #B7C0C8;
+        color: #b7c0c8;
         font-size: 10px;
         letter-spacing: 0.05em;
         text-transform: uppercase;
@@ -40,7 +43,7 @@ export default {
     &__message {
         margin-top: 1px;
         font-size: 13px;
-        color: #7D8790;
+        color: #7d8790;
     }
 }
 </style>
