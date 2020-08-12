3b3e28634e5d41f99c7657461c5e7786e41a1338
diatomiccoder <chudak_93@live.com>
2020-08-12 22:14:01 +0500

    remove extra component in main app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
     <div id="app">
         <div class="container">
-            <Chat />
+            <div class="chat">
+                <nav class="chat__sidebar">
+                    <ChatList :chats="chat" />
+                </nav>
+                <main class="chat__dialog">
+                    <router-view></router-view>
+                </main>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
     name: 'App',
+    computed: {
+        ...mapGetters(['chat'])
+    },
     methods: {
         ...mapActions(['getChat', 'getUser', 'getUsers'])
     },
@@ -19,7 +30,7 @@ export default {
         this.getUsers();
     },
     components: {
-        Chat: () => import('./components/Chat.vue')
+        ChatList: () => import('./components/ChatList.vue')
     }
 };
 </script>
@@ -27,7 +38,7 @@ export default {
 <style lang="scss" scoped>
 #app {
     min-height: 100vh;
-    background-color: #E5E5E5;
+    background-color: #e5e5e5;
 
     display: flex;
     align-items: center;
@@ -38,5 +49,27 @@ export default {
     padding: 0 15px;
     max-width: 1200px;
     width: 100%;
+}
+
+.chat {
+    background-color: #f3f6f8;
+    max-width: 980px;
+    max-height: 100vh;
+    height: 720px;
+
+    margin: 0 auto;
+    display: flex;
+
+    &__sidebar {
+        flex: 1;
+        max-width: 300px;
+    }
+
+    &__dialog {
+        flex: 1;
+        max-width: 680px;
+        background-color: #fff;
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+    }
 }
 </style>
